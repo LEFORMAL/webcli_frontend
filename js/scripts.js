ecfcc519ba2c57bd0c137b0c1ec9092a0a6a402b
cristobal ortiz venegas <cri.ortizv@duocuc.cl>
2024-11-13 01:07:43 -0300

    añadir servicio

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,7 @@ function loadSidebar() {
             sidebarHtml += `
                 <a href="admin_dashboard.html">Panel de Administración</a>
                 <a href="crea_user.html">Crear técnico o gestor</a>
+                <a href="add_service.html">Añadir nuevo servicio</a>
             `;
         }
 
