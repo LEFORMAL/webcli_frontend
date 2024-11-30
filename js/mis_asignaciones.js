db46b6c18966c50a28b8e96283850f86e3f3f9ad
cristobal ortiz venegas <cri.ortizv@duocuc.cl>
2024-11-29 23:58:58 -0300

    añadir fecha a mis asignaciones tecnico

--- js/mis_asignaciones.js
+++ js/mis_asignaciones.js
@@ -43,10 +43,13 @@ async function cargarAsignaciones() {
             guardarBtn.classList.add('btn-guardar');
             guardarBtn.addEventListener('click', () => actualizarEstado(asignacion.ID_SOLICITUD, estadoSelect.value, asignacionItem));
 
+            const fechareali = new Date(solicitud.FECHA_REALIZACION).toLocaleDateString();
+
             // Agregar la información y controles a la solicitud
             asignacionItem.innerHTML = `
                 <p><strong>Nombre Cliente:</strong> ${asignacion.NOMBRE}</p>
                 <p><strong>Tipo de Solicitud:</strong> ${asignacion.TIPO_SOLICITUD}</p>
+                <p><strong>Fecha de realización:</strong> ${fechaFormateada}</p>
                 <p><strong>Dirección:</strong> ${asignacion.DIRECCION}</p>
                 <p><strong>Marca:</strong> ${asignacion.MARCA_PRODUCTO}</p>
                 <p><strong>Modelo:</strong> ${asignacion.MODELO_PRODUCTO}</p>
